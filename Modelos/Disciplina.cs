01f8cf7c8dd1735aa19ce2f980f2712c23483624
Gabriel Xavier <111456243+gxavier1@users.noreply.github.com>
2022-10-09 21:14:24 +0000

    nona aula

--- Modelos/Disciplina.cs
+++ Modelos/Disciplina.cs
@@ -1,4 +1,4 @@
-namespace Modelos {
+.namespace Modelos {
 
   class Disciplina {
     
--- Modelos/Impressora.cs
+++ Modelos/Impressora.cs
@@ -4,57 +4,22 @@ namespace Modelos {
 
   class Impressora {
 
-    public static void Imprimir(Object obj) {
-      Console.WriteLine("-------------------------");
-      if (obj is Pessoa) {
-        Imprimir((Pessoa) obj);
-      }
-      if (obj is Professor) {
-        Imprimir((Professor) obj);
-      }
-      if (obj is Aluno) {
-        Imprimir((Aluno) obj);
-      }
-      if (obj is Disciplina) {
-        Imprimir((Disciplina) obj);
-      }
-      if (obj is Contato) {
-        Imprimir((Contato) obj);
-      }
-      Console.WriteLine("-------------------------");
+    public delegate string Linha();
+
+    public static string LinhaComum() {
+      return "-------------------------";
     }
 
-    private static void Imprimir(Pessoa pessoa) {
-      Console.WriteLine(
-        "Nome: " + pessoa.Nome + "\n" +
-        "CPF: " + pessoa.Cpf
-      );
-    }
-
-    private static void Imprimir(Contato contato) {
-      Console.WriteLine(
-       contato.Informar()
-      );
-    }
-
-    private static void Imprimir(Professor professor) {
-      Console.WriteLine(
-        "Codigo: " + professor.Codigo
-      );
+    public static void Imprimir(Texto texto) {
+      Imprimir(texto, LinhaComum, LinhaComum);
     }
     
-    private static void Imprimir(Aluno aluno) {
-      Console.WriteLine(
-        "Matricula: " + aluno.Matricula + "\n" +
-        "Periodo: " + aluno.Periodo
-      );
-    }
-
-    private static void Imprimir(Disciplina disciplina) {
-      Console.WriteLine(
-        "Nome: " + disciplina.Nome + "\n" +
-        "Semestre: " + disciplina.Semestre
-      );
+    public static void Imprimir(Texto texto,
+                                Linha cabecalho,
+                                Linha rodape) {
+      Console.WriteLine(cabecalho());
+      Console.WriteLine(texto.Informar());
+      Console.WriteLine(rodape());
     }
     
   }
